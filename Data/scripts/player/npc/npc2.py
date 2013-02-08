8b26ac59689458eccba952a9fae0353ddb8b9e3f
guitaristocrats <guitaristocrats@yahoo.com>
2013-02-08 13:23:04 +0000

    After fixing the way shops.cfg is parsed by removing a bug in the for loop's logic, I have now begun work on adding all right-clickable stores to the game. All Catherby stores have newly been added (along with their respective NPCs), and various stores throughout the world now work. On top of that I figured out how the script manager works (at least for NPCs and object), and I've begun work migrating all second click stores to npc2.py. Hopefully this makes up for yesterday's lack of substantial updates.

--- Data/scripts/player/npc/npc2.py
+++ Data/scripts/player/npc/npc2.py
@@ -1,17 +1,77 @@
+def npcClick2_520(c, npcId):
+	c.getShops().openShop(22)
+
+def npcClick2_521(c, npcId):
+	c.getShops().openShop(22)
+
 def npcClick2_522(c, npcId):
 	c.getShops().openShop(1)
 
 def npcClick2_523(c, npcId):
 	c.getShops().openShop(1)
 
-def npcClick2_546(c, npcId):
-	c.getShops().openShop(7)
+def npcClick2_526(c, npcId):
+	c.getShops().openShop(9)
 
-def npcClick2_548(c, npcId):
-	c.getShops().openShop(8)
+def npcClick2_527(c, npcId):
+	c.getShops().openShop(9)
+
+def npcClick2_530(c, npcID):
+	c.getShops().openShop(1)
 
 def npcClick2_537(c, npcId):
 	c.getShops().openShop(9)
 
+def npcClick2_540(c, npcId):
+	c.getShops().openShop(20)
+
+def npcClick2_542(c, npcId):
+	c.getShops().openShop(23)
+
+def npcClick2_544(c, npcId):
+	c.getShops().openShop(24)
+
+def npcClick2_546(c, npcId):
+	c.getShops().openShop(5)
+
+def npcClick2_548(c, npcId):
+	c.getShops().openShop(6)
+
+def npcClick2_549(c, npcId):
+	c.getShops().openShop(4)
+
+def npcClick2_550(c, npcId):
+	c.getShops().openShop(3)
+
+def npcClick2_553(c, npcId):
+	c.getShops().openShop(8)
+
+def npcClick2_563(c, npcId):
+	c.getShops().openShop(1)
+
+def npcClick2_575(c, npcId):
+	c.getShops().openShop(39)
+
+def npcClick2_576(c, npcId):
+	c.getShops().openShop(38)
+
+def npcClick2_577(c, npcId):
+	c.getShops().openShop(12)
+
+def npcClick2_581(c, npcId):
+	c.getShops().openShop(14)
+
 def npcClick2_582(c, npcId):
 	c.getShops().openShop(48)
+
+def npcClick2_584(c, npcId):
+	c.getShops().openShop(13)
+
+def npcClick2_595(c, npcId):
+	c.getShops().openShop(101)
+
+def npcClick2_836(c, npcId):
+	c.getShops().openShop(25)
+
+def npcClick2_2305(c, npcId):
+	c.getShops().openShop(104)
